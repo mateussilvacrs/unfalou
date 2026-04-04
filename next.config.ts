7ebfed9fae7393b1b84378e32f6dbcfd3f30dd31
Mateus <mateus_silva139@hotmail.com>
2026-04-04 05:36:14 -0300

    version 0.5 beta

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   /* config options here */
 
   images: {
-    qualities: [10, 20,30,40,50,60,70,80,100],
+    qualities: [10, 20,30,40,50,60,70,80,100]
   },
   
 };
