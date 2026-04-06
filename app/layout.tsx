79a6d6e728d964006124c5aa23c7c04019a4e1db
Mateus <mateus_silva139@hotmail.com>
2026-04-06 14:21:34 -0300

    Add donations and sliders and news styles version 1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
