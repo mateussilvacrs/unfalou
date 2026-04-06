5bc92b6e9d6ef60c908db9fe67e827c89b1f3079
Mateus <mateus_silva139@hotmail.com>
2026-04-06 14:18:53 -0300

    Add donations and sliders and news styles version 1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import "@/globals.css";
+import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Header from "@/components/Header";
